f90272798a063ea05730f58fcec415417c81c4c3
xinedison <ecehuangying@sz.pku.edu.cn>
2015-11-03 10:23:16 +0800

    valid input and output file

--- src/imageIO/imageInOut.cpp
+++ src/imageIO/imageInOut.cpp
@@ -6,17 +6,22 @@
  *      read ,write and show  image like matlab format
  */
 
+#include <stdexcept>
 #include <cv.h>
 #include <highgui.h>
 
 namespace acf{
 
-using namespace std;
+using std::string;
+using std::invalid_argument;
 using cv::Mat;
 
-Mat imread(char* filename){
+Mat imread(const char* filename){
 	Mat image;
-	image = cv::imread(filename,1);
+	image = cv::imread(filename,CV_LOAD_IMAGE_COLOR);
+	if(!image.data){
+		throw invalid_argument(string(filename)+" can not  be open or read in!");
+	}
 	return image;
 }
 
@@ -24,7 +29,7 @@ void imwrite(const string& filename,Mat image){
 	cv::imwrite(filename,image);
 }
 
-void imshow(Mat image,const string  windowName="figure"){
+void imshow(const Mat image,const string  windowName="figure"){
 	cv::namedWindow(windowName,CV_WINDOW_AUTOSIZE);
 	cv::imshow(windowName,image);
 	cv::waitKey(0);
@@ -34,9 +39,14 @@ void imshow(Mat image,const string  windowName="figure"){
 
 
 int main( int argc, char** argv ){
-	cv::Mat image;
-	image = acf::imread( argv[1] );
-	acf::imshow(image);
-	acf::imwrite("test.jpg",image);
+	try{
+		cv::Mat image;
+		image = acf::imread( argv[1] );
+		acf::imshow(image);
+		acf::imwrite("test.jpg",image);
+	}
+	catch(const std::invalid_argument& ia){
+		std::cout << argv[1] << " is not a valid for read!" << std::endl;
+	}
 	return 0;
 }
