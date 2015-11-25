462baa3f25a53b2c2a17554b4856cdcf9254723b
Zhenqiang.Ying <yingzhenqiang@gmail.com>
2015-11-25 20:50:47 +0800

    add color channel, not support param:smooth yet, build passing

---
+++ src/features/chnsCompute/Channel.cpp
@@ -0,0 +1,13 @@
+﻿
+#include "Channel.h"
+
+void ColorChn::compute(void){
+	// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	// % compute color channels
+	// p=pChns.pColor; nm='color channels';
+	// I=rgbConvert(I,p.colorSpace); I=convTri(I,p.smooth);
+	// if(p.enabled), chns=addChn(chns,I,nm,p,'replicate',h,w); end
+	// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	cvtColor(img, img, colorSpace);
+	// smoothing is not available now.
+}
